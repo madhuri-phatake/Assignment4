2a07641e31e9f852a6ede0bef88cb4275c7263dc
madhuri605 <80457300+madhuri605@users.noreply.github.com>
2022-08-02 14:15:55 +0530

    new commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,32 @@
+<!-- <template>
+<div class="bg-green-400 m-44">
+
+    <p class="text-center text-red-400 text-3xl ">Welcome to our Nuxt-crud-page</p>
+    <div class="m-8 items-center ">
+        <nuxt-link to="login" class="border border-gray-400 px-4 py-2 max-w-xs rounded bg-teal-600 text-white ">Login</nuxt-link>
+    </div>
+
+    <p class=" text-xl text-gray-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, asperiores assumenda quae culpa quidem consequuntur laborum est, iste, voluptate magnam ullam exercitationem architecto quod pariatur vitae eveniet earum molestias. Ducimus harum ratione, omnis veritatis totam quod sit quo? Iusto aspernatur dolore modi voluptate error et soluta consequatur, facilis perspiciatis non. Ipsam ex ad quod, vel ut totam ratione enim sunt.</p>
+</div>
 
 
 
-     <template>
+</template> -->
+
+ <template>
          <div class="flex items-center justify-center min-h-screen bg-gray-100">
         <div class="px-8 py-6 mt-4 text-left bg-white shadow-lg">
                 <p class="text-2xl font-bold m-5 ">
                 Login
                 </p>
     
-                <form action="components/card.vue" method="post">
-                    <input v-model="input.username" aria-label="Enter your email address" 
-                        type="text" placeholder="User Name" 
+                <div >
+                    <input v-model="name" aria-label="Enter your email address" 
+                        type="name" placeholder="User Name" 
                         class="text-sm text-gray-base w-full 
                                 mr-3 py-5 px-4 h-2 border 
                                 border-gray-200 rounded mb-2" />
-                    <input v-model="input.password" aria-label="Enter your password" 
+                    <input v-model="password" aria-label="Enter your password" 
                         type="password" placeholder="Password"
                         class="text-sm text-gray-base w-full mr-3 
                                 py-5 px-4 h-2 border border-gray-200 
@@ -25,68 +37,53 @@
                         type="button"
                         >
                         Submit -->
-                        <button type="button" @click="login" class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-                        >Submit</button>
+                        <button @click="login" class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"  >Submit</button>
                 </div>
         
-                </form>
+                </div>
             </div>
             </div>
-            <Card/>
+            
     </template>
 
-    <!-- <script>
-// import { sign } from 'crypto';
- import axios from "axios"
+
+
+<script>
+import axios from "axios"
 export default{
-    name:"sign",
-    data()
-    {
-        return {
-            name:'',
-            email:'',
-            password:'',
+    name:"login",
+    data(){
+        return{
+            name:"",
+            password:""
         }
     },
     methods:{
-        async sign(){
-            console.warn("this is called",this.name,this.password)
-            let result= await axios.post("http://localhost:3000/users",{
-                name:this.name,
-                password:this.password
-            })
-            console.warn(result);
-            if(result.status==201){
-                alert("sign up done");
+        async login(){
+            let result=await axios.get(
+                `http://localhost:3000/users?email=${this.name}&password=${this.password}`
+            );
+            if(result.status==200 && result.data.length>0){
+                this.$router.push({name:"bucket"})
             }
-}
+            else{
+                alert("please enter correct name and password")
+            }
+            
+            }
     }
 }
-</script> -->
-<script>
-export default {
-        name: 'Login',
-        data() {
-            return {
-                input: {
-                    username: "",
-                    password: ""
-                }
-            }
-        },
-        methods: {
-            login() {
-                if(this.input.username != "" && this.input.password != "") {
-                    if(this.input.username == this.$parent.mockAccount.username && this.input.password == this.$parent.mockAccount.password) {
-                        this.$emit("authenticated", true);
-                        this.$router.replace({ name: "secure" });
-                    } else {
-                        console.log("The username and / or password is incorrect");
-                    }
-                } else {
-                    console.log("A username and password must be present");
-                }
-            }
-        }
-    }
 </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
